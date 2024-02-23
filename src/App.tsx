559dbee11bb0d6ad94620e474cda8f7c6bc789eb
Beksultan Dzhienbekov <bekadj14@gmail.com>
2024-02-23 19:12:59 +0600

    сделал импорт стора

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import './App.css';
-import store from "./redux/store";
+import { store } from "./redux/store";
 import KeyboardSimulator from "./components/KeyboardSimulator.tsx";
 
 const App = () => {
