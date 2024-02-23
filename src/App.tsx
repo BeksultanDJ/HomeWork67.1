ba3a1ac9413654e939b0c69c68bd7155f634a334
Beksultan Dzhienbekov <bekadj14@gmail.com>
2024-02-23 18:08:30 +0600

    испортировал в другой кокмонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import React, { useState } from 'react';
-import { createStore } from 'redux';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import './App.css';
-
-const SET_PIN = 'SET_PIN';
-const CLEAR_PIN = 'CLEAR_PIN';
-
-const setPin = (digit: string) => {
-    return { type: SET_PIN, payload: digit };
-};
-
-const clearPin = () => {
-    return { type: CLEAR_PIN };
-};
-
-const pinReducer = (state = '', action: any) => {
-    switch (action.type) {
-        case SET_PIN:
-            return state.length < 4 ? state + action.payload : state;
-        case CLEAR_PIN:
-            return '';
-        default:
-            return state;
-    }
-};
-
-const store = createStore(pinReducer);
+import store from './components/KeyboardSimulator.tsx'
+import KeyboardSimulator from "./components/KeyboardSimulator.tsx";
 
 const App = () => {
     return (
