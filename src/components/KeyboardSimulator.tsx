db379616e745aa1b93fbcdcb66ef9ea081de6a44
Beksultan Dzhienbekov <bekadj14@gmail.com>
2024-02-23 19:18:42 +0600

    изменил импорт стора изменил устловие для handleEnter, accesMwssage и для обработки пароля изменил доступ к pin

--- src/components/KeyboardSimulator.tsx
+++ src/components/KeyboardSimulator.tsx
@@ -3,12 +3,11 @@ import { useState } from "react";
 import { setPin, clearPin } from "../redux/action/pinActions.ts";
 import AccessMessage from "./AccessMessage.tsx";
 import Keyboard from "./Keyboard.tsx";
-import store from '../redux/store';
-
+import { store } from '../redux/store';
 
 const KeyboardSimulator = () => {
     const dispatch = useDispatch();
-    const pin = useSelector((state: string) => state);
+    const pin = useSelector((state) => state.pin);
 
     const correctPin = '9999';
     const [accessGranted, setAccessGranted] = useState(false);
@@ -19,7 +18,7 @@ const KeyboardSimulator = () => {
     };
 
     const handleEnter = () => {
-        if (pin === correctPin) {
+        if (pin !== undefined && pin === correctPin) {
             setAccessGranted(true);
             setShowAccessMessage(true);
         } else {
@@ -34,15 +33,15 @@ const KeyboardSimulator = () => {
     };
 
     return (
-        <Provider store={store}> {/* Provide the store to the application */}
+        <Provider store={store}>
             <div>
                 {accessGranted ? (
                     <AccessMessage message="Access Granted" color="green" />
-                ) : pin.length === 4 && showAccessMessage ? (
+                ) : pin !== undefined && pin.length === 4 && showAccessMessage ? (
                     <AccessMessage message="Access Denied" color="red" />
                 ) : null}
                 <div>
-                    <div>Password: {pin.replace(/./g, '*')}</div>
+                    <div>Password: {pin !== undefined ? pin.replace(/./g, '*') : ''}</div>
                     <Keyboard
                         onButtonClick={handleButtonClick}
                         onClear={handleClear}
