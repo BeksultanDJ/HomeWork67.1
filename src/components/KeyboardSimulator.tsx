78dbeca8b7a44c9ebe6f151a1136677cf66bcd67
Beksultan Dzhienbekov <bekadj14@gmail.com>
2024-02-23 18:21:45 +0600

    импортироал стор и редюсер событий

--- src/components/KeyboardSimulator.tsx
+++ src/components/KeyboardSimulator.tsx
@@ -1,32 +1,10 @@
 import { useDispatch, useSelector, Provider } from "react-redux";
 import { useState } from "react";
-import { createStore } from "redux";
+import { setPin, clearPin } from "../redux/action/pinActions.ts";
 import AccessMessage from "./AccessMessage.tsx";
 import Keyboard from "./Keyboard.tsx";
+import store from '../redux/store';
 
-const SET_PIN = 'SET_PIN';
-const CLEAR_PIN = 'CLEAR_PIN';
-
-const setPin = (digit: string) => {
-    return { type: SET_PIN, payload: digit };
-};
-
-const clearPin = () => {
-    return { type: CLEAR_PIN };
-};
-
-const pinReducer = (state = '', action: any) => {
-    switch (action.type) {
-        case SET_PIN:
-            return state.length < 4 ? state + action.payload : state;
-        case CLEAR_PIN:
-            return '';
-        default:
-            return state;
-    }
-};
-
-const store = createStore(pinReducer);
 
 const KeyboardSimulator = () => {
     const dispatch = useDispatch();
